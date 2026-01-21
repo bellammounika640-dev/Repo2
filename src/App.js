9b3e266e7c4df727ca13a1dda403d233a4cb9d8f
K.Tejaswini <tejaswinikunchala9@gmail.com>
2026-01-21 15:52:15 +0530

    add jewellery images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Places from './component/Places';
+import React from 'react';
+import Jewellery from './component/Jewellery';
 
 function App() {
   return (
     <div className="App">
      <Places/>
+     <Jewellery/>
     </div>
   );
 }
---
+++ src/component/Jewellery.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Jewellery() {
+  return (
+    <div>
+      <h1 style={{color:"purple"}}><ins>Famous Jewellery In Hyderabad</ins></h1>
+      <div className='div1'>
+        <h1>1.Pearl Jewellery</h1>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzFzriRXnEeF2f--rVmP8Q3n0iTPKnwTwM9A&s" alt=''></img>
+      </div>
+      <div className='div1'>
+        <h1>2.Kundan & Polki</h1>
+        <img src="https://images.jdmagicbox.com/v2/comp/def_content/ncat_id/polki-jewellery-q7itmf2-250.jpg" alt=''></img>
+      </div>
+      <div className='div1'>
+        <h1>3.Enamelled Jewellery (Meenakari)</h1>
+        <img src="https://www.maharaniwomen.com/wp-content/uploads/2022/07/149679.jpg" alt=''></img>
+      </div>
+    </div>
+  )
+}
+
+export default Jewellery
