bd64ecb7931a52ec43115d4c4187f6b839b280a5
mounika <bellammounka640@gmail.com>
2026-01-21 15:24:37 +0530

    adding place images

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Places from './component/Places';
+
+function App() {
+  return (
+    <div className="App">
+     <Places/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Places.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Places() {
+  return (
+    <div>
+      <h1 style={{color:"green"}}><ins> Best Places In Hyderabad </ins></h1>
+      <div className='div1'>
+      <h1>1.Birla Mandir</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Birla_Mandir%2C_Hyderabad.png" alt=''></img>
+      </div>
+      <div className='div1' >
+      <h1>2.Charminar</h1>
+      <img src="https://www.indiatourismguide.in/wp-content/uploads/2017/11/Charminar.jpg" alt=''></img>
+      </div>
+      <div className='div1'>
+        <h1>3.Golkonda Fort</h1>
+        <img src="https://s7ap1.scene7.com/is/image/incredibleindia/golconda-fort-hyderabad-secunderabad-telangana-1-musthead-hero?qlt=82&ts=1742170130583" alt=''></img>
+      </div>
+    </div>
+  )
+}
+
+export default Places
+
+
